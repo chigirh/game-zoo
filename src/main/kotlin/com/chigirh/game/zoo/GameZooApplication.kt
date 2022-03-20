e3a97ddb1dccf8cab680319f2684cd380fa2e5ed
chigirh <abch9431@gmail.com>
2022-03-20 10:10:13 +0900

    first commit

---
+++ src/main/kotlin/com/chigirh/game/zoo/GameZooApplication.kt
@@ -0,0 +1,11 @@
+package com.chigirh.game.zoo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GameZooApplication
+
+fun main(args: Array<String>) {
+	runApplication<GameZooApplication>(*args)
+}
---
+++ src/test/kotlin/com/chigirh/game/zoo/GameZooApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chigirh.game.zoo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GameZooApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
